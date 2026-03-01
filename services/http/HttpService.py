5dc09e39e3e879bd5e9a144b23172580270fd6bb
Joshua Webb <jwebb98@live.com>
2026-03-01 16:26:58 -0600

    Fixes an issue where Request parameters weren't being passed properly and code clean up

--- services/http/HttpService.py
+++ services/http/HttpService.py
@@ -1,6 +1,6 @@
 from .IHttpService import IHttpService
 from .models.HttpResponse import HttpResponse
-from urllib.parse import urlencode, urljoin, urlparse
+from urllib.parse import urlencode, urljoin, urlparse, ParseResult
 from urllib.response import addinfourl
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
@@ -9,19 +9,18 @@ class HttpService(IHttpService):
     """
     HTTP service client for a single server.
 
-    URLs are built by combining the base URL with relative URLs.
+    Absolute URLs are built by combining the base URL with relative URLs.
 
     Parameters
     ----------
-    base_url : str
-        The base URL to be used with every request. Should be passed **without**
-        a trailing forward slash. Example: ``https://example.com``.
+    base_url : str, required
+        The base URL to be used with every request. Should be passed **without** a trailing forward slash.  
+        Example: ``https://example.com``
     default_headers : dict, optional
-        Default headers to include with every request. Example: 
-        ``{"User-Agent": "Example"}``.
+        Default headers to include with every request.  
+        Example: ``{"User-Agent": "Example"}``
     timeout : float, optional
-        Timeout in seconds for blocking operations (e.g., connecting to the server,
-        reading data). Defaults to 1.0.
+        Timeout in seconds for blocking operations (e.g., connecting to the server, reading data). Defaults to 1.0
     """
     def __init__(
         self,
@@ -29,9 +28,9 @@ class HttpService(IHttpService):
         default_headers: dict[str, str] | None = None,
         timeout: float = 1.0,
     ):
-        self.base_url = base_url.rstrip("/")
-        self.default_headers = default_headers
-        self.timeout = timeout
+        self.base_url: str = base_url.rstrip("/")
+        self.default_headers:dict[str, str] | None = default_headers
+        self.timeout: float = timeout
     
     def _build_url(self, rel_url: str):
         if not self.base_url:
@@ -43,26 +42,29 @@ class HttpService(IHttpService):
         return urljoin(self.base_url + "/", rel_url.lstrip("/"))
     
     def _build_headers(self, additional_headers: dict | None) -> dict:
-        if not self.default_headers:
+        if not additional_headers and not self.default_headers:
+            return {}
+        
+        if additional_headers and not self.default_headers:
             return additional_headers
         
-        if not additional_headers and self.default_headers:
+        if self.default_headers and not additional_headers:
             return self.default_headers
         
-        all_headers = self.default_headers.copy()
+        all_headers: dict = self.default_headers.copy()
         all_headers.update(additional_headers)
         
         return all_headers
     
-    def _build_parameters(self, abs_url, parameters):
+    def _build_parameters(self, abs_url: str, parameters: dict[str, str]):
         # Add request parameters if exists
         if parameters:
             return abs_url + "?" + urlencode(parameters, safe=",")
         
         return abs_url
     
-    def _is_valid_relative_url(self, rel_url):
-        parsed = urlparse(rel_url)
+    def _is_valid_relative_url(self, rel_url: str):
+        parsed: ParseResult = urlparse(rel_url)
         if parsed.scheme or parsed.netloc or parsed.query:
             return False
         
@@ -82,16 +84,25 @@ class HttpService(IHttpService):
             Additional headers to combine with default_headers to be sent with the request.  
             Example: ``{"User-Agent": "Example"}``
 
-        parameters : dict[str, str], optional
+        params : dict[str, str], optional
             Dictionary of query parameters to append to the URL.  
-            Example: ``{"Name": "John"}``.
+            Example: ``{"Name": "John"}``
         """
         # Hardening
         if not relative_url is None and not isinstance(relative_url, str):
             raise TypeError(f"Invalid 'url' parameter type. Received {type(relative_url)}, but expected str")
         
-        if not headers is None and not isinstance(headers, dict):
-            raise TypeError(f"Invalid 'headers' parameter type. Received {type(headers)}, but expected dict[str, str]")
+        if not headers is None:
+            if not isinstance(headers, dict):
+                raise TypeError(f"Invalid 'headers' parameter type. Received {type(headers)}, but expected dict[str, str]")
+            
+            # Enfore dict[str, str]
+            for key, value in headers.items():
+                if not isinstance(key, str):
+                    raise ValueError(f"The header key {repr(key)} is an invalid type. Received {type(key)}, but expected str")
+                
+                if not isinstance(value, str):
+                    raise ValueError(f"The header value {repr(value)} is an invalid type. Received {type(value)}, but expected str")
         
         if not params is None and not isinstance(params, dict):
             raise TypeError(f"Invalid 'params' parameter type. Received {type(params)}, but expected dict[str, str]")
@@ -101,11 +112,10 @@ class HttpService(IHttpService):
         
         request_url = self._build_url(relative_url)
         request_url = self._build_parameters(request_url, params)
-        print(request_url)
         headers = self._build_headers(headers)
         
         # Try to send the request
-        request = Request(request_url, headers, method="GET")
+        request = Request(url=request_url, headers=headers, method="GET")
         response = None
         try:
             response: addinfourl = urlopen(request, timeout=self.timeout)
--- services/http/models/HttpResponse.py
+++ services/http/models/HttpResponse.py
@@ -7,8 +7,14 @@ class HttpResponse():
     headers: dict[str, str]
     body: str | bytes
 
-    def text(self, encoding: str = "utf-8"):
+    def body_to_text(self, encoding: str = "utf-8"):
+        """Returns the raw text of the response body"""
         return self.body.encode(encoding)
     
-    def json(self):
+    def body_to_json_object(self) -> dict:
+        """Returns a subscriptable json object from the response body"""
         return json.loads(self.body)
+    
+    def body_to_json_text(self) -> str:
+        """Returns a JSON formatted string from the response body"""
+        return json.dumps(json.loads(self.body))
